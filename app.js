2363725bd842ec499385a280aef075e2a7d6ff30
Yash Goswami <yashgoswami148@gmail.com>
2020-05-25 13:20:07 +0530

    complete put and patch

--- app.js
+++ app.js
@@ -52,6 +52,57 @@ app.route("/articles")
     });
 });
 
+app.route("/articles/:articleTitle")
+.get(function(req,res){
+    const selectedArticle = req.params.articleTitle;
+    Article.findOne({title:selectedArticle},function(err,foundArticle){
+        if(!err){
+            res.send(foundArticle);
+        }else{
+            res.send("No articles mathcing that title was found");
+        }
+    });
+})
+.put(function(req,res){
+    Article.updateOne(
+        {title:req.params.articleTitle},
+        {title:req.body.title,content:req.body.content},
+        {overwrite:true},
+        function(err){
+            if(!err){
+                res.send("Successfully updated article");
+            }else{
+                res.send(err);
+            }
+        }
+    )
+})
+.patch(function(req,res){
+    Article.updateOne(
+        {title:req.params.articleTitle},
+        {$set:req.body},
+        function(err){
+            if(!err){
+                res.send("Successfully updated the articles.");
+            }else{
+                res.send(err);
+            }
+        }
+    )
+})
+.delete(function(req,res){
+    Article.deleteOne(
+        {title:req.body.title},
+        function(err){
+            if(!err){
+                res.send("Successfully deleted the article");
+            }else{
+                res.send(err);
+            }
+        }
+    )
+});
+
 app.listen(3000,function(){
     console.log("Server started on port 3000");
 });
